93d14b4a63c2fd2207bc90174eb4b16a69d3ed92
Tadej Svetina <tadej.svetina@jina.ai>
2021-08-10 16:20:01 +0200

    feat: add json output

--- src/utils/benchmark.py
+++ src/utils/benchmark.py
@@ -1,45 +1,40 @@
-from pathlib import Path
-from typing import Any, Callable
+from statistics import mean, stdev
+from typing import Any, Callable, Dict, Iterable, Optional, Tuple
 
-from jina import __version__
-
-
-def get_output_file_name(current_file: str) -> str:
-    """Get the output file name based on the name of ``current_file``
-
-    The output file name will be based on the jina version and the
-    name you passed with ``current_file`` - you should use ``__file__``
-    for that. For example, if your file is::
-
-        src/document_array_append.py
-
-    and you are working with jina 2.0.17, the function will return::
-
-        docs/static/artifacts/2.0.17/document_array_append.txt
-    """
-
-    output_dir = Path.cwd() / f'docs/static/artifacts/{__version__}'
-    output_dir.mkdir(parents=True, exist_ok=True)
-    output_file_name = output_dir / f'{Path(current_file).stem}.txt'
-
-    return output_file_name
+from .timecontext import TimeContext
 
 
-def average_benchmark(
-    func: Callable[[Any], float], description: str, n: int, *args, **kwargs
+def benchmark_time(
+    func: Callable[[Any], Any],
+    n: int,
+    setup: Optional[Callable[[Any], Tuple[Iterable, Dict[str, Any]]]] = None,
+    args: Optional[Tuple] = None,
+    kwargs: Optional[Dict] = None,
 ) -> str:
-    """Get average benchmark result over multiple repetitions
+    """Get average time and std by benchmarking a function multiple times
 
     :param func: The function to benchmark
-    :param description: the description of the result, should contain
-        ``{avg_time}`` in it, which will be filled with the result
-        and returned
+    :param setup: A setup function that can perform setup before running
+        the ``func``. It should take as inputs the ``args`` and ``kwargs``
+        that you provided, and return a tuple of an iterable, which will
+        be used to provide ``args`` to ``func``, and a dictionary, which
+        will be used to provide ``kwargs`` to ``func``.
     :param n: Number of repetitions
-    :param args: Positional arguments to pass to ``func``
-    :param kwargs: Keyword arguments to pass to ``func``
+    :param args: Positional arguments to pass to ``func`` (or ``setup``)
+    :param kwargs: Keyword arguments to pass to ``func`` (or ``setup``)
     """
 
-    results = [func(*args, **kwargs) for _ in range(n)]
-    avg_time = sum(results) / len(results)
+    results = []
+    args = args if args is not None else ()
+    kwargs = kwargs if kwargs is not None else {}
 
-    return description.format(avg_time=avg_time)
+    for i in range(n):
+        if setup is not None:
+            args, kwargs = setup(*args, **kwargs)
+
+        with TimeContext() as t:
+            func(*args, **kwargs)
+
+        results.append(t.duration)
+
+    return mean(results), stdev(results)
