2b6dc8c5aa4cdca130af01c0779b269c0b07ee7b
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-08-10 18:55:42 +0200

    feat: benchmark match metadata

--- src/document_array_match.py
+++ src/document_array_match.py
@@ -1,4 +1,4 @@
-from typing import Dict, Generator, Tuple, Union
+from typing import Dict, Generator, Union
 
 import numpy as np
 import pytest
@@ -11,7 +11,7 @@ NUM_REPETITIONS = 5
 
 
 def _generate_docs_with_embs(
-    n_docs: int, emb_size: int
+        n_docs: int, emb_size: int
 ) -> Generator[Document, None, None]:
     embedings = np.random.random((n_docs, emb_size))
     for emb in embedings:
@@ -19,110 +19,69 @@ def _generate_docs_with_embs(
 
 
 def match_arrays(
-    array1: Union[DocumentArray, DocumentArrayMemmap],
-    array2: Union[DocumentArray, DocumentArrayMemmap],
-    topk: int,
-    metric: str,
-    use_scipy: bool,
+        array1: Union[DocumentArray, DocumentArrayMemmap],
+        array2: Union[DocumentArray, DocumentArrayMemmap],
+        topk: int,
+        metric: str,
+        use_scipy: bool,
 ):
     array1.match(array2, limit=topk, metric=metric, use_scipy=use_scipy)
 
 
-def prepare_inputs_standard(
-    size1: int = 10,
-    size2: int = 10_000,
-    emb_size: int = 128,
-    topk: int = 10,
-    metric: str = 'cosine',
-    use_scipy: bool = False,
+def _prepare_inputs_standard(
+        size1: int = 10,
+        size2: int = 10_000,
+        emb_size: int = 128,
+        topk: int = 10,
+        metric: str = 'cosine',
+        use_scipy: bool = False,
+        dam_x: bool = False,
+        dam_y: bool = False,
+        dam_path: str = './'
 ) -> Dict:
+    if not dam_x:
+        x = DocumentArray(_generate_docs_with_embs(size1, emb_size))
+    else:
+        x = DocumentArrayMemmap(f'{dam_path}/x')
+        x.extend(_generate_docs_with_embs(size1, emb_size))
+    if not dam_y:
+        y = DocumentArray(_generate_docs_with_embs(size2, emb_size))
+    else:
+        y = DocumentArrayMemmap(f'{dam_path}/y')
+        y.extend(_generate_docs_with_embs(size2, emb_size))
     return dict(
-        array1=DocumentArray(_generate_docs_with_embs(size1, emb_size)),
-        array2=DocumentArray(_generate_docs_with_embs(size2, emb_size)),
+        array1=x,
+        array2=y,
         topk=topk,
         metric=metric,
         use_scipy=use_scipy,
     )
 
 
-def prepare_inputs_self(size: int = 10, emb_size: int = 128, topk: int = 10) -> Dict:
-    docs = DocumentArray(_generate_docs_with_embs(size, emb_size))
-    return dict(
-        array1=docs,
-        array2=docs,
-        topk=topk,
-        metric='cosine',
-        use_scipy=False,
-    )
-
-
-def setup_match(**kwargs) -> Tuple[Tuple, Dict]:
-    array1 = kwargs["array1"]
-    array2 = kwargs["array2"]
-
-    for arr in (array1, array2):
-        for doc in arr:
-            doc.pop('matches')
-
-    return (), kwargs
-
-
-@pytest.mark.parametrize('size1', [10, 100])
-@pytest.mark.parametrize('size2', [10_000, 100_000])
-def test_array_sizes(size1: int, size2: int, json_metadata):
-
-    mean_time, std_time = benchmark_time(
-        match_arrays,
-        NUM_REPETITIONS,
-        setup=setup_match,
-        kwargs=prepare_inputs_standard(size1=size1, size2=size2),
-    )
-
-    json_metadata["mean_time"] = mean_time
-    json_metadata["std_time"] = std_time
-
-
-@pytest.mark.parametrize('emb_size', [512, 1024])
-def test_emb_sizes(emb_size: int, json_metadata):
-
-    mean_time, std_time = benchmark_time(
-        match_arrays,
-        NUM_REPETITIONS,
-        setup=setup_match,
-        kwargs=prepare_inputs_standard(emb_size=emb_size),
-    )
-
-    json_metadata["mean_time"] = mean_time
-    json_metadata["std_time"] = std_time
-
-
+@pytest.mark.parametrize('size_X', [100, 1000])
+@pytest.mark.parametrize('size_Y', [1000, 10000, 100000])
+@pytest.mark.parametrize('dam_x', [True])
+@pytest.mark.parametrize('dam_y', [True, False])
+@pytest.mark.parametrize('emb_size', [256, 1024])
 @pytest.mark.parametrize('use_scipy', [True, False])
 @pytest.mark.parametrize('metric', ['cosine', 'euclidean', 'sqeuclidean'])
-def test_metrics(use_scipy: bool, metric: str, json_metadata):
-    if not use_scipy and metric == 'cosine':
-        pytest.skip('Skipping repeated default configuration')
-
+def test_match(size_X: int, size_Y: int, dam_x: bool, dam_y: bool, emb_size: int, use_scipy: bool, metric: str, tmpdir,
+               json_metadata):
     mean_time, std_time = benchmark_time(
         match_arrays,
         NUM_REPETITIONS,
-        setup=setup_match,
-        kwargs=prepare_inputs_standard(metric=metric, use_scipy=use_scipy),
+        kwargs=_prepare_inputs_standard(size1=size_X, size2=size_Y, metric=metric, use_scipy=use_scipy, dam_x=dam_x,
+                                        dam_y=dam_y, dam_path=str(tmpdir)),
     )
 
     json_metadata["mean_time"] = mean_time
     json_metadata["std_time"] = std_time
-
-
-@pytest.mark.parametrize('size', [100, 1000])
-@pytest.mark.parametrize('topk', [10, 100])
-def test_match_against_self(size: int, topk: int, json_metadata):
-
-    mean_time, std_time = benchmark_time(
-        match_arrays,
-        NUM_REPETITIONS,
-        setup=setup_match,
-        kwargs=prepare_inputs_self(size=size, topk=topk),
-    )
-
-    json_metadata["mean_time"] = mean_time
-    json_metadata["std_time"] = std_time
+    json_metadata["description"] = "match between different arrays"
+    json_metadata["repetitions"] = NUM_REPETITIONS
+    json_metadata["metric"] = metric
+    json_metadata["use_scipy"] = use_scipy
+    json_metadata["size_X"] = size_X
+    json_metadata["size_Y"] = size_Y
+    json_metadata["dam_x"] = dam_y
+    json_metadata["dam_y"] = dam_y
+    json_metadata["emb_size"] = emb_size
