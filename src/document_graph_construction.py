df3bea77b27afb0cd3f78f7b16ac08091914c4a8
David Buchaca Prats <davidbuchaca@gmail.com>
2021-08-27 19:31:02 +0200

    fix: removing redundant test

--- src/document_graph_construction.py
+++ src/document_graph_construction.py
@@ -80,36 +80,6 @@ def test_graph_add_edges_assuming_all_nodes_present(n_nodes, n_edges, json_write
 
 @pytest.mark.parametrize('n_edges', [2000, 20_000])
 @pytest.mark.parametrize('n_nodes', [1000, 10_000])
-def test_graph_add_edges_assuming_no_nodes_present(n_nodes, n_edges, json_writer):
-
-    docs = [Document(text=f'Document{i}') for i in range(n_nodes)]
-    sources = [random.choice(docs) for i in range(n_edges)]
-    targets = [random.choice(docs) for i in range(n_edges)]
-    edge_features = [
-        {'text': f'I connect Doc{i} and Doc{j}'} for i, j in zip(sources, targets)
-    ]
-
-    def _build_graph_doc():
-        graph = GraphDocument()
-        graph.add_edges(sources, targets, edge_features=edge_features)
-        return graph
-
-    mean_time, std_time = benchmark_time(func=_build_graph_doc, n=NUM_REPETITIONS)
-
-    json_writer.append(
-        dict(
-            name='document_graph_construction/test_graph_add_edges_assuming_no_nodes_present',
-            iterations=NUM_DOCS,
-            mean_time=mean_time,
-            std_time=std_time,
-            unit='ms',
-            metadata=dict(n_nodes=n_nodes, n_edges=n_edges),
-        )
-    )
-
-
-@pytest.mark.parametrize('n_edges', [2000, 20_000])
-@pytest.mark.parametrize('n_nodes', [1000, 10_000])
 def test_graph_add_single_edge_assuming_all_nodes_present(
     n_nodes, n_edges, json_writer
 ):
