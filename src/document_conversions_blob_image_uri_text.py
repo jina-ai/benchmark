26bcf7aac2c0f9fc1a54b752462cd1fd16568513
bwanglzu <bo.wang@jina.ai>
2021-11-22 18:45:46 +0100

    fix: adjust all tests in benchmark

--- src/document_conversions_blob_image_uri_text.py
+++ src/document_conversions_blob_image_uri_text.py
@@ -11,17 +11,17 @@ from .pages import Pages
 """
 This file contains tests for the following methods from Document:
 
-- convert_image_uri_to_blob
+- load_uri_to_image_blob
 - convert_image_buffer_to_blob
 - convert_image_datauri_to_blob
 - convert_buffer_to_blob
 - convert_image_blob_to_uri
 - convert_blob_to_buffer
-- convert_uri_to_buffer
+- load_uri_to_buffer
 - convert_uri_to_datauri
 - convert_buffer_to_uri
 - convert_text_to_uri
-- convert_uri_to_text
+- load_uri_to_text
 - convert_content_to_uri
 """
 
@@ -30,16 +30,16 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 @pytest.mark.parametrize("num_docs", [1, 100, 1000])
-def test_document_convert_image_uri_to_blob(num_docs, json_writer):
+def test_document_load_uri_to_image_blob(num_docs, json_writer):
     def _input_docs():
         image_dir = os.path.join(cur_dir, "utils", "test.png")
         return (), dict(docs=[Document(uri=image_dir) for _ in range(num_docs)])
 
-    def _image_uri_to_blob(docs):
+    def _load_uri_to_image_blob(docs):
         for doc in docs:
-            doc.convert_image_uri_to_blob()
+            doc.load_uri_to_image_blob()
 
-    result = benchmark_time(setup=_input_docs, func=_image_uri_to_blob)
+    result = benchmark_time(setup=_input_docs, func=_load_uri_to_image_blob)
 
     json_writer.append(
         page=Pages.DOCUMENT_CONVERSION,
@@ -61,7 +61,7 @@ def test_document_convert_uri_to_buffer(num_docs, json_writer):
 
     def _convert_uri_to_buffer(docs):
         for doc in docs:
-            doc.convert_uri_to_buffer()
+            doc.load_uri_to_buffer()
 
     result = benchmark_time(setup=_input_docs, func=_convert_uri_to_buffer)
 
@@ -79,14 +79,14 @@ def test_document_convert_image_buffer_to_blob(num_docs, json_writer):
         docs = []
         for _ in range(num_docs):
             doc = Document(uri=image_dir)
-            doc.convert_uri_to_buffer()
+            doc.load_uri_to_buffer()
             docs.append(doc)
 
         return (), dict(docs=docs)
 
     def _image_buffer_to_blob(docs):
         for doc in docs:
-            doc.convert_image_buffer_to_blob()
+            doc.convert_buffer_to_image_blob()
 
     result = benchmark_time(setup=_input_docs, func=_image_buffer_to_blob)
 
@@ -111,7 +111,7 @@ def test_document_convert_image_datauri_to_blob(num_docs, json_writer):
 
     def _convert_image_datauri_to_blob(docs):
         for doc in docs:
-            doc.convert_image_datauri_to_blob()
+            doc.load_uri_to_image_blob()
 
     result = benchmark_time(setup=_input_docs, func=_convert_image_datauri_to_blob)
 
@@ -187,7 +187,7 @@ def test_document_convert_image_blob_to_uri(num_docs, json_writer):
 
     def _convert_image_blob_to_uri(docs):
         for doc in docs:
-            doc.convert_image_blob_to_uri(32, 28)
+            doc.convert_image_blob_to_uri()
 
     result = benchmark_time(setup=_input_docs, func=_convert_image_blob_to_uri)
 
@@ -239,7 +239,7 @@ def test_document_convert_text_to_uri(num_docs, json_writer):
 
     def _convert_text_to_uri(docs):
         for doc in docs:
-            _ = doc.convert_text_to_uri
+            _ = doc.dump_text_to_datauri
 
     result = benchmark_time(setup=_input_docs, func=_convert_text_to_uri)
 
@@ -277,7 +277,7 @@ def test_document_convert_buffer_to_uri(num_docs, json_writer):
 
 
 @pytest.mark.parametrize("num_docs", [1, 5])
-def test_document_convert_uri_to_text(num_docs, json_writer):
+def test_document_load_uri_to_text(num_docs, json_writer):
     def _input_docs():
         return (
             (),
@@ -289,11 +289,11 @@ def test_document_convert_uri_to_text(num_docs, json_writer):
             ),
         )
 
-    def _convert_uri_to_text(docs):
+    def _load_uri_to_text(docs):
         for doc in docs:
-            _ = doc.convert_uri_to_text()
+            _ = doc.load_uri_to_text()
 
-    result = benchmark_time(setup=_input_docs, func=_convert_uri_to_text)
+    result = benchmark_time(setup=_input_docs, func=_load_uri_to_text)
 
     json_writer.append(
         page=Pages.DOCUMENT_CONVERSION,
